698fc8ede24a6666b58827e6230bb198787ad6a7
azuki774 <azuki774s@gmail.com>
2025-06-21 10:57:56 +0900

    fix parameters

--- model/parameters.go
+++ model/parameters.go
@@ -1,6 +1,6 @@
 package model
 
-const PopulationSize int = 100     // 一世代の集団のサイズ
+const PopulationSize int = 500     // 一世代の集団のサイズ
 const MaxGeneration int = 500000   // 何世代行うか
-const MutationRate float64 = 0.005 // 突然変異率
+const MutationRate float64 = 0.01 // 突然変異率
 const ShowLogInterval int = 1000   // どの頻度でログを出力するか
